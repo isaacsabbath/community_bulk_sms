1009c28a094fab7090e3310ec3d6fe5ab2516ce2
Isaac Sabath <126468761+isaacsabbath@users.noreply.github.com>
2025-08-23 03:55:33 +0300

    Update settings.py

--- communtiy/settings.py
+++ communtiy/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 # settings.py
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '319405420cbc.ngrok-free.app']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '']
 
 import os
 # Application definition
